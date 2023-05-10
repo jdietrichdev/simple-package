af652831ccfda6e015070bf18ff0d3d02ab184b5
Josh Dietrich <jdietrich2016@gmail.com>
2023-05-10 11:59:46 -0500

    fix(setup): removing code to trigger release

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ export const test = () => {
   return 'Hello world';
 }
 
-export const TEST_VALUE = 'test';
-
 export default {
-  test,
-  TEST_VALUE
+  test
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import { assert } from 'chai';
-import { test, TEST_VALUE } from '../src/index';
+import { test } from '../src/index';
 import npmPackage from '../src/index';
 
 describe('NPM package', () => {
@@ -11,10 +11,6 @@ describe('NPM package', () => {
   it('should have a test property', () => {
     assert.property(npmPackage, 'test');
   });
-
-  it('should have a TEST_VALUE property', () => {
-    assert.property(npmPackage, 'TEST_VALUE');
-  })
 });
 
 describe('Test function', () => {
@@ -25,15 +21,5 @@ describe('Test function', () => {
   it('should return hello world', () => {
     const response = test();
     assert.equal(response, 'Hello world');
-  });
-});
-
-describe('Test value', () => {
-  it('should be a string', () => {
-    assert.isString(TEST_VALUE);
-  });
-
-  it('should equal test', () => {
-    assert.equal(TEST_VALUE, 'test');
   });
 });
